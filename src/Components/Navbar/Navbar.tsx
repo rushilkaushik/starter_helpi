cd1aeaf7fef317b53998de561adaa996082a8817
Edmar Pelayo <edmarpelayo@yahoo.com>
2024-04-16 12:12:58 -0400

    Added Props in Navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
-interface Props{};
+interface Props {}
 
-export function Navbar() {
-    return (
-        <>
-            <Link to="/">
-                <button>Home</button>
-            </Link>
-            <Link to="/alternate">
-                <button>Alternate</button>
-            </Link>
-            <Link to="/basicq">
-                <button>BasicQuestionsPage</button>
-            </Link>
-            <Link to="/detailedq">
-                <button>DetailedQuestionsPage</button>
-            </Link>
-        </>
-    )
+export function Navbar(props: Props) {
+  return (
+    <>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+      <Link to="/alternate">
+        <button>Alternate</button>
+      </Link>
+      <Link to="/basicq">
+        <button>BasicQuestionsPage</button>
+      </Link>
+      <Link to="/detailedq">
+        <button>DetailedQuestionsPage</button>
+      </Link>
+    </>
+  );
 }
 
 export default Navbar;
