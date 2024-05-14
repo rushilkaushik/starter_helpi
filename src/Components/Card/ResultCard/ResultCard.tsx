d9845888e511a61697cfd6cf0c1361085df068f5
Edmar Pelayo <edmarpelayo@yahoo.com>
2024-05-14 15:38:59 -0400

    Added Functionality to the project

--- src/Components/Card/ResultCard/ResultCard.tsx
+++ src/Components/Card/ResultCard/ResultCard.tsx
@@ -27,14 +27,29 @@ const ResultCard: React.FC<ResultCardProps> = ({ resultData }) => {
         justifyContent: "space-between",
       }}
     >
-      <div className="bg-white shadow-md rounded-lg p-8 w-full md:w-2/3 lg:w-3/4 xl:w-2/3 ml-auto mr-auto pt-10 border border-gray-300">
+      <div className="bg-white shadow-md rounded-lg p-8 w-full md:w-2/3 lg:w-3/4 xl:w-2/3 ml-auto mr-auto pt-10 border border-gray-300 flex flex-col items-center">
         {/* Remove justify-between class to line up elements vertically */}
-        {dataArray.map((item, index) => (
-          <p key={index}>
-            <span>{index + 1}. </span>
-            {item}
-          </p>
-        ))}
+        {/* <h1> Stuff: {trimmedResultData}</h1> */}
+        <h1 className="text-2xl font-bold mb-4">Suggested career path:</h1>
+        <p>{dataArray[1]}</p>
+        <h1 className="text-2xl font-bold mb-4">Type of schooling required:</h1>
+        <p>{dataArray[2]}</p>
+
+        <h1 className="text-2xl font-bold mb-4">
+          Estimated time to become qualified:
+        </h1>
+        <p>{dataArray[3]}</p>
+
+        <h1 className="text-2xl font-bold mb-4">Salary information:</h1>
+        <p>{dataArray[4]}</p>
+
+        <h1 className="text-2xl font-bold mb-4">Job demand:</h1>
+        <p>{dataArray[5]}</p>
+
+        <h1 className="text-2xl font-bold mb-4">
+          Reasons why this job would be suitable for them:
+        </h1>
+        <p>{dataArray[6]}</p>
       </div>
     </div>
   );
